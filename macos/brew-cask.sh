2bd35244de6cd36e2c4778320f8b9ea98ac59edd
Rex Tsou <rex.tsou@kkday.com>
2018-07-14 11:42:34 +0800

    Update brew-cask.sh

--- macos/brew-cask.sh
+++ macos/brew-cask.sh
@@ -2,23 +2,26 @@
 
 brew tap caskroom/cask
 
+# brew cask install darktable
+# brew cask install filezilla
+# brew cask install keybase
+# brew cask install ngrok
+# brew cask install sourcetree
+# brew cask install spotify
+# brew cask install telegram
+# brew cask install visual-studio-code
+# brew cask install vlc
+# brew cask install xquartz
+brew cask install docker
 brew cask install font-hanamina
 brew cask install font-noto-sans-cjk
 brew cask install font-noto-serif-cjk
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install karabiner-elements
-# brew cask install sourcetree
-# brew cask install spotify
-# brew cask install telegram
+brew cask install snipaste
 brew cask install vagrant
 brew cask install virtualbox
-brew cask install docker
-brew cask install snipaste
-# brew cask install visual-studio-code
-# brew cask install vlc
-# brew cask install xquartz
-brew cask install keybase
 
 brew cask cleanup
 brew cask doctor
