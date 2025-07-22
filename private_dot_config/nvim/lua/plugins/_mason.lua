ac466bf8858c704eb28dc243088551199581f7f4
Rex Tsou <akccakccwww@gmail.com>
2025-07-22 21:18:55 +0800

    Update nvim mason config

--- private_dot_config/nvim/lua/plugins/_mason.lua
+++ private_dot_config/nvim/lua/plugins/_mason.lua
@@ -107,51 +107,37 @@ M.config = function()
 	--     - on_attach: a lua callback function to run after LSP atteches to a given buffer
 	local lspconfig = require('lspconfig')
 
+	local opts = { noremap = true, silent = true }
 	vim.keymap.set('n', 'K', function()
 		vim.lsp.buf.hover { border = 'rounded', max_height = 25, max_width = 120}
 	end)
-
-	-- vim.keymap.set('n', '<Leader>i', function()
-	-- 	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({0}),{0})
-	-- end)
+	-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+	vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = false }) end, opts)
+	vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = false }) end, opts)
+	vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+	vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+	vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, opts)
+	vim.keymap.set('n', '<space>f', function()
+		vim.lsp.buf.format({ async = true })
+	end, opts)
 
 	-- Use an on_attach function to only map the following keys
 	-- after the language server attaches to the current buffer
 	local on_attach = function(_, bufnr)
-
-		-- Customized on_attach function
-		-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-		local opts = { noremap = true, silent = true, buffer = bufnr }
-		vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1, float = false }) end, opts)
-		vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1, float = false }) end, opts)
-		vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-		vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
 		-- Enable completion triggered by <c-x><c-o>
 		vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local bufopts = { noremap = true, silent = true, buffer = bufnr }
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-		vim.keymap.set('n', 'K', function()
-			vim.lsp.buf.hover { border = 'rounded', max_height = 25, max_width = 120}
-		end, bufopts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-		-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-		vim.keymap.set('n', '<space>wl', function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, bufopts)
-		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-		vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-		vim.keymap.set('n', '<space>f', function()
-			vim.lsp.buf.format({ async = true })
-		-- end, bufopts)
-		end, opts)
 	end
 
 	-- set lspconfig default options
